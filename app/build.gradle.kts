2cf8e833560b1e199d17b9669fe0fe2023743245
rahibladex <rahibladex@github.com>
2026-08-25 19:29:32 +0530

    Remove unused broken layout files causing resource linking errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.acousticguard"
         minSdk = 24
         targetSdk = 34
-        versionCode = 13
-        versionName = "3.2.0"
+        versionCode = 14
+        versionName = "3.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
