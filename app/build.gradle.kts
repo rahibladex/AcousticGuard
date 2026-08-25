93ca48dbbcce504094a485be968e5015cdf393cc
rahibladex <rahibladex@github.com>
2026-08-25 17:19:59 +0530

    Bump version to 3.1.0 for NariShakti SOS

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.acousticguard"
         minSdk = 24
         targetSdk = 34
-        versionCode = 11
-        versionName = "3.0.0"
+        versionCode = 12
+        versionName = "3.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
