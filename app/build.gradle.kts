89f53d650b2b89a7607086b9f83902422d3069f3
rahibladex <rahibladex@github.com>
2026-08-25 19:36:41 +0530

    Add clean step and debug logging to CI/CD

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.acousticguard"
         minSdk = 24
         targetSdk = 34
-        versionCode = 14
-        versionName = "3.3.0"
+        versionCode = 15
+        versionName = "3.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
